6629b0cb8e2eb5eb0b76e24c604542919735a3f4
slava <vyacheslav.plishkin@gmail.com>
2021-03-28 15:53:04 +0300

    @truffle/hdwallet-provider downgraded to 1.2.3 version to get rinkeby work

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,12 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
+const secret = require("./secret.json");
 
 module.exports = {
   /**
@@ -72,6 +73,15 @@ module.exports = {
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
+    rinkeby: {
+      provider: () => new HDWalletProvider(secret.mnemonic, `https://rinkeby.infura.io/v3/${secret.projectId}`),
+      network_id: 4,       // Rinkeby's id
+      gas: 8500000,
+      gasPrice: 1000000000,  // 1 gwei (in wei) (default: 100 gwei)
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
   },
 
   // Set default mocha options here, use special reporters etc.
